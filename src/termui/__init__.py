8485263cc6dfbcee8d3fa883235cc1933736009d
medu <meduvada63@gmail.com>
2026-09-04 17:15:19 +0530

    chore: expose TermUI public API

---
+++ src/termui/__init__.py
@@ -0,0 +1,6 @@
+"""Small, dependency-free terminal UI primitives."""
+
+from .core import box, color, gradient, progress, table, visible_len
+
+__all__ = ["box", "color", "gradient", "progress", "table", "visible_len"]
+__version__ = "1.0.0"
